76590f66722a58e01560c8ecf72ce9ce676e86f0
siamislam07 <siamislam1970@gmail.com>
2023-11-06 13:51:54 +0600

    basic stracture ready , connected to mongodb and install some dependencies

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express()
+require('dotenv').config()
+const port = process.env.PORT || 5000
+
+
+//uri
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.amxcsfu.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        // app.get('/api/services', (req, res) => {
+        //     res.send("get api is running")
+        // })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+
+
+app.listen(port, () => {
+    console.log(`Server port running on  ${port}`)
+})
